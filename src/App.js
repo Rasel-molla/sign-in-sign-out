882be9144681167789ab700f0b6551c4c951e9ba
Rasel-molla <raselabir107@gmail.com>
2021-06-01 14:47:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebaseConfig';
+import { useState } from 'react';
+
+firebase.initializeApp(firebaseConfig);
+
+
 
 function App() {
+
+
+const [user,setUser] = useState({
+  isSignedIn:false,
+  name:'',
+  email:'',
+  photo:''
+})
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+ const handleSignIn= () =>{
+  //  console.log('Cllik');
+ firebase.auth().signInWithPopup(provider)
+.then (res=>{
+const {displayName,photoURL,email} = res.user;
+const signedUser = {
+isSignedIn : true,
+name: displayName,
+email:email,
+photo: photoURL
+}
+setUser(signedUser)
+
+  console.log(displayName,photoURL,email);
+
+})
+.catch(err => {
+  console.log(err);
+})
+}
+const handleSignOut = () => {
+firebase.auth().signOut()
+.then(res =>{
+const signedOutUser ={
+  isSignedIn: false,
+
+   name:'',
+   email:'',
+   photo:''
+
+
+}
+setUser(signedOutUser);
+  // console.log(res);
+})
+.catch(err =>{
+
+
+})
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{  
+user.isSignedIn ? <button onClick={handleSignOut}>Sign out </button>
+:
+<button onClick={handleSignIn}>Sign in </button>
+}
+  {
+    user.isSignedIn && <div>
+<p>Welcome : {user.name}</p>
+<h6>email:{user.email}</h6>
+<img src={user.photo} alt="" />
+    </div>
+  }
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCddG87TOiwEb2q4Tm0oyX4sNgnF-ZjEgs",
+    authDomain: "ema-john-simple-129fa.firebaseapp.com",
+    projectId: "ema-john-simple-129fa",
+    storageBucket: "ema-john-simple-129fa.appspot.com",
+    messagingSenderId: "891609749048",
+    appId: "1:891609749048:web:c1829c261c8f81177b38cd"
+  };
+
+  export default firebaseConfig;
